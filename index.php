60c247cc3f99821dec0d54f0247d2e1d3c770e51
LucaPierantoni <luca.pier99@gmail.com>
2024-06-18 16:45:40 +0200

    UPD table done

--- index.php
+++ index.php
@@ -3,39 +3,39 @@
     $hotels = [
 
         [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
+            'Name' => 'Hotel Belvedere',
+            'Description' => 'Hotel Belvedere Descrizione',
+            'Parking' => true,
+            'Vote' => 4,
+            'Distance_to_center' => 10.4
         ],
         [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
+            'Name' => 'Hotel Futuro',
+            'Description' => 'Hotel Futuro Descrizione',
+            'Parking' => true,
+            'Vote' => 2,
+            'Distance_to_center' => 2
         ],
         [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
+            'Name' => 'Hotel Rivamare',
+            'Description' => 'Hotel Rivamare Descrizione',
+            'Parking' => false,
+            'Vote' => 1,
             'distance_to_center' => 1
         ],
         [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
+            'Name' => 'Hotel Bellavista',
+            'Description' => 'Hotel Bellavista Descrizione',
+            'Parking' => false,
+            'Vote' => 5,
+            'Distance_to_center' => 5.5
         ],
         [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
+            'Name' => 'Hotel Milano',
+            'Description' => 'Hotel Milano Descrizione',
+            'Parking' => true,
+            'Vote' => 2,
+            'Distance_to_center' => 50
         ],
 
     ];
@@ -54,57 +54,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1>Hotel Php</h1>
+<body class="d-flex flex-column align-items-center">
+    <h1 class="my-2">Hotel php</h1>
 
-    <?php
-
-        foreach($hotels as $currentHotel) {
-            echo "
-            <li>
-                <ul>
-                    ";
+    <table class="table w-75 border border-secondary my-3">
+        <thead>
+            <tr class="table-primary">
+            <?php
+                foreach ($keys as $key) {
+                    echo "<th scope=\"col\">$key</th>";
+                }
+            ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($hotels as $currentHotel) {
+                    echo "
+                    <tr class=\"table-secondary\">";
                     foreach($currentHotel as $key => $value) {
-                        echo "
-                        <li>
-                            $key:  $value
-                        </li>";
+
+                        if($key == 'Parking') {
+                            $value = ($value) ? 'Si' : 'No';
+                        }
+
+                        echo "<td>$value</td>";
                     }
                     echo "
-                </ul>                    
-            </li>
-            ";
-        }
-    ?>
-    <table class="table">
-    <thead>
-        <tr>
-        <?php
-            foreach ($keys as $key) {
-                echo "<th scope=\"col\">$key</th>";
-            }
-        ?>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>ciao</td>
-        <td>ciao</td>
-        <td>ciao</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>ciao</td>
-        <td>ciao</td>
-        <td>ciao</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td colspan="2">ciao</td>
-        <td>ciao</td>
-        </tr>
-    </tbody>
+                    </tr>";
+                }
+            ?>
+        </tbody>
     </table>
 </body>
 </html>
